5aeb040dfe47d313fbb00b47e6750786e4b0d166
Daniele Del Piano <daniele.delpiano30@gmail.com>
2023-07-08 16:20:39 +0100

    refactored the http interceptor

--- frontend/src/app/core/interceptors/http-error.interceptor.ts
+++ frontend/src/app/core/interceptors/http-error.interceptor.ts
@@ -7,8 +7,8 @@ import {
     HTTP_INTERCEPTORS
 } from '@angular/common/http'
 import { Router } from '@angular/router'
-import { throwError } from 'rxjs'
-import { catchError } from 'rxjs/operators'
+import { throwError, timer } from 'rxjs'
+import { catchError, retry } from 'rxjs/operators'
 import { NotificationService } from '../services/notification.service'
 
 @Injectable()
@@ -20,13 +20,39 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         return next.handle(req).pipe(
-            catchError((error: HttpErrorResponse) => {
-                if (error.status === 401) {
-                    this.router.navigateByUrl('/')
-                    this.notifier.showError(error.error.message)
-                    console.log(error.error.message)
+            catchError((error: unknown) => {
+                if (error instanceof HttpErrorResponse) {
+                    if (error instanceof ErrorEvent) {
+                        console.warn('Error event', error)
+                    } else {
+                        console.warn(
+                            `error status : ${error.status}  ${error.statusText}`
+                        )
+                        switch (error.status) {
+                            case 401:
+                                this.router.navigateByUrl('/')
+                                this.notifier.showError(error.error.message)
+                                break
+                            case 403:
+                                this.router.navigateByUrl('/')
+                                this.notifier.showError(error.error.message)
+                                break
+                            case 0:
+                                retry({
+                                    count: 3,
+                                    delay: (_, retryCount) =>
+                                        timer(retryCount * 1000)
+                                })
+                                this.router.navigateByUrl('/')
+                                this.notifier.showError('Server is down')
+                        }
+                    }
+                } else {
+                    console.warn(error)
+                    this.notifier.showError(
+                        'Error was detected! We are already working on it!'
+                    )
                 }
-
                 return throwError(() => error)
             })
         )
